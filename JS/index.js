deff5898f750696f8b7cc9d788e57c144f288eb3
Jinyu Wu <q835771840@gmail.com>
2019-08-31 15:08:26 -0500

    finished browser, need to finish async issue

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,65 @@
-let intro = `<span id="face"> 😄 </span>Josiah Wu<br>
+function load_intro() {
+  // intro module
+  let emoji = '<span id="face">😄</span>';
+  let name = "Josiah Wu";
+  let intro = `${emoji} ${name} <br>
 <span style='color:#c5050c;;'>University of Wisconsin - Madison</span> <br>
 that's right, I am a badger engineer 🦡<br>
-I like coding, learning, and running.<br>"`,
-  i = 0,
-  isTag,
-  text;
+I like coding, learning, and running."`,
+    i = 0,
+    isTag,
+    text;
+  (function type() {
+    text = intro.slice(0, ++i);
+    if (text === intro) return;
+    $("#intro").html(text);
+    var char = text.slice(-1);
+    if (char === "<") isTag = true;
+    if (char === ">") isTag = false;
 
-(function type() {
-  text = intro.slice(0, ++i);
-  if (text === intro) return;
+    if (isTag) return type();
+    setTimeout(type, 25);
+  })();
+  $("#start_button").click(function() {
+    $(this).hide();
+    walk_through();
+  });
+}
+load_intro();
 
-  document.getElementById("intro").innerHTML = text;
+function walk_through() {
+  let intro = $("#intro");
+  let stage = $("#stage");
+  update_intro("I can make website responsive");
+  scrollTo("#stage");
+  stage.slideDown("slow");
+  dance_browser(false);
+  function update_intro(txt) {
+    intro.fadeOut("fast", function() {
+      $(this).text('" ' + txt + ' "');
+      $(this).fadeIn("fast");
+    });
+  }
+}
 
-  var char = text.slice(-1);
-  if (char === "<") isTag = true;
-  if (char === ">") isTag = false;
+function scrollTo(selector) {
+  document.querySelector(selector).scrollIntoView({ behavior: "smooth" });
+}
 
-  if (isTag) return type();
-  setTimeout(type, 25);
-})();
-
+//make browser dance
+function dance_browser(grow) {
+  let flip_card = $(".flip-card");
+  let w = flip_card.width();
+  let inte = setInterval(() => {
+    change_width(flip_card, w);
+    if (grow && w > 1200) clearInterval(inte);
+    else if (!grow && w < 375) {
+      clearInterval(inte);
+      dance_browser(true);
+    }
+    grow ? (w += 2) : (w -= 2);
+  }, 1);
+  function change_width(ele, width) {
+    ele.width(width + "px");
+  }
+}
