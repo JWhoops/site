a1ee5c726d5f35ad79f4f3d637f0b017bee6d248
Jinyu Wu <q835771840@gmail.com>
2019-09-01 22:48:05 -0500

    finish first version

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,18 @@
 function load_intro() {
   // intro module
-  let emoji = "Hi 😄<br>";
-  let name = "Josiah Wu";
-  let intro = `${emoji} I'm ${name} <br>
-University of Wisconsin - Madison <br>
-that's right, I am a badger engineer 🦡<br>
-I like coding, learning, and running."`,
+  let stop = false;
+  let intro = `Hi 😄<br> I'm Josiah Wu <br>
+A senior student in University of Wisconsin - Madison <br>
+That's right, I am a badger engineer 🦡<br>
+I like coding, learning, and running.<br>Current Status:
+<br> Actively looking for a full time software engineering
+position for 2020<br> Check my <a target="_blank" href="./meta/Josiah_Wu.pdf">Resume</a> for contact info.😄
+"`,
     i = 0,
     isTag,
     text;
   (function type() {
+    if (stop) return;
     text = intro.slice(0, ++i);
     if (text === intro) return;
     $("#intro").html(text);
@@ -20,47 +23,50 @@ I like coding, learning, and running."`,
     if (isTag) return type();
     setTimeout(type, 25);
   })();
+
   $("#start_button").click(function() {
     $(this).fadeOut("fast");
     walk_through();
+    stop = true;
   });
 }
+
 load_intro();
 
 async function walk_through() {
   let stage = $("#stage");
-  let page = $("#browser_page");
+  let browser_content = $("#browser_content");
   let intro = $("#intro");
   let browser_back = $("#browser_back");
   let browser_front = $("#browser_front");
   update_intro("For front-end:");
   await pause(2000);
   update_intro("I built responsive website like this.");
+  browser_content.attr("src", "https://q835771840.github.io/The-Next-Web/");
   stage.slideDown("slow");
   await pause(1000);
   await dancing_browser(false, 2);
   update_intro("And responsive fluid design.");
   await pause(1000);
-  page.removeAttr("data");
-  page.attr("data", "https://q835771840.github.io/Newsweek/");
+  browser_content.attr("src", "https://q835771840.github.io/Newsweek/");
   await pause(1500);
   await dancing_browser(false, 2);
   await pause(1000);
   update_intro("I also know a little bit of UI design, like tear down design.");
   await pause(1500);
-  page.attr("data", "https://q835771840.github.io/Teardown-Design/");
+  browser_content.attr("src", "https://q835771840.github.io/Teardown-Design/");
   await pause(3000);
-  page.attr("data", "");
+  browser_content.attr("src", "");
   update_intro(
     "Since I am a full-stack developer, I also know back-end skills, here is my skill list."
   );
   stage.css("width", "600px");
   stage.css("border", "none");
-  skill_list();
+  list_skills();
   await pause(2000);
   rotate_browser();
 
-  function skill_list() {
+  function list_skills() {
     browser_front.append(skill_col(["HTML", "CSS", "JavaScript", "ES6"]));
     browser_front.append(skill_col(["Bootstrap", "Semantic UI", "SASS"]));
     browser_front.append(skill_col(["React", "Redux", "Optimization"]));
