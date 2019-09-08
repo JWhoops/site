21c3fa5d8cf7fa5a68118c8850420b275340929e
Jinyu Wu <q835771840@gmail.com>
2019-09-07 21:43:29 -0500

    added project list

--- JS/index.js
+++ JS/index.js
@@ -32,31 +32,29 @@ position for 2020<br> Check my <a target="_blank" href="./meta/Josiah_Wu.pdf">Re
 }
 
 load_intro();
-
 async function walk_through() {
   let stage = $("#stage");
+  let browser = $("#browser");
   let browser_content = $("#browser_content");
   let intro = $("#intro");
-  let browser_back = $("#browser_back");
-  let browser_front = $("#browser_front");
+  let browser_back = $("#card-container");
   update_intro("For front-end:");
   await pause(2000);
   update_intro("I built responsive website like this.");
   browser_content.attr("src", "https://q835771840.github.io/The-Next-Web/");
   stage.slideDown("slow");
   await pause(1000);
-  // await dancing_browser(false, 2);
-  stage.css("width", "375px");
+  browser.css("width", "375px");
   await pause(4000);
-  stage.css("width", "1000px");
+  browser.css("width", "100%");
   await pause(4000);
   update_intro("And responsive fluid design.");
   await pause(1000);
   browser_content.attr("src", "https://q835771840.github.io/Newsweek/");
   await pause(1500);
-  stage.css("width", "375px");
+  browser.css("width", "375px");
   await pause(4000);
-  stage.css("width", "1000px");
+  browser.css("width", "100%");
   await pause(5000);
   update_intro("I also know a little bit of UI design, like tear down design.");
   await pause(1500);
@@ -64,22 +62,11 @@ async function walk_through() {
   await pause(3000);
   browser_content.attr("src", "");
   update_intro(
-    "Since I am a full-stack developer, I also know back-end skills, here is my skill list."
+    "Since I am a full-stack developer, I also know back-end skills, here is my project list."
   );
-  stage.css("width", "600px");
-  stage.css("border", "none");
-  list_skills();
-  await pause(2000);
+  browser_back.append(get_all_skills());
+  browser_back.css("width", "630px");
   rotate_browser();
-
-  function list_skills() {
-    browser_front.append(skill_col(["HTML", "CSS", "JavaScript", "ES6"]));
-    browser_front.append(skill_col(["Bootstrap", "Semantic UI", "SASS"]));
-    browser_front.append(skill_col(["React", "Redux", "Optimization"]));
-    browser_back.append(skill_col(["Node.js", "Java", "PHP", "Ruby"]));
-    browser_back.append(skill_col(["Express", "Ruby On Rails"]));
-    browser_back.append(skill_col(["MongoDB", "MySQL", "SQLite"]));
-  }
 
   // utility function
   function update_intro(txt) {
@@ -103,23 +90,69 @@ function pause(sec) {
   });
 }
 
-function skill_col(skill_cards) {
-  let sc = '<div class="column">';
-  sc +=
-    skill_cards
-      .map(n => {
-        return skill(n);
-      })
-      .join("") + "</div>";
-  return sc;
+function get_all_skills() {
+  let skills = [
+    {
+      name: "MadMap",
+      description: `Since UW-Madison is huge, I built and
+      designed this website that can help new students of
+      UW-Madison find nearby utilities like Microwave, Printer,
+      and Lab.`,
+      stack: "HTML, CSS, Javascript, Node.js, Express, MongoDB, and Heroku",
+      link: "https://q835771840.github.io/Madmap/"
+    },
+    {
+      name: "Jianshu",
+      description: `A web application I built to practice my React and Redux
+      development skills.`,
+      stack: "React and Redux",
+      link: "https://github.com/q835771840/Jianshu"
+    },
+    {
+      name: "Jhin",
+      description: `A grid-based CSS framework I created for responsive design.`,
+      stack: "SASS and CSS Flex-box",
+      link: "https://github.com/q835771840/jHin"
+    },
+    {
+      name: "Yelp Camp",
+      description: `A web application to practice my full stack development skill.`,
+      stack: "Express.js, MongoDB, and Node.js, passport.js",
+      link: "https://github.com/q835771840/yelp-camp"
+    },
+    {
+      name: "Neck Guard",
+      description: `A web-based mobile application that can help people protect their neck.`,
+      stack: "MUI, Javascript, HTML, CSS",
+      link: "https://github.com/q835771840/NeckG"
+    }
+  ];
+
+  return skills
+    .map(s => {
+      return skill(s);
+    })
+    .join("");
 }
 
-function skill(name) {
+function skill(obj) {
   return `
   <div class="card">
-      <h3 class="card_title">
-        ${name}
-      </h3>
+  <div class="content">
+    <div class="header">${obj.name}</div>
+    <div class="description">
+      Project Description: ${obj.description}
+    </div>
+    <div id="tech-stack">
+      Tech Stack: ${obj.stack}
+    </div>
   </div>
+  <a target='_blank' href="${obj.link}">
+    <div id="skill-button" class="ui bottom attached button">
+      <i class="github icon"></i>
+      Visit Project
+    </div>
+  </a>
+</div>
   `;
 }
